5daf746f83d1b30c29a81a4f95304e6b40d3c94d
DESKTOP-U2N87OI\Yigitcan <yigitcanozturk@ogr.bandirma.edu.tr>
2021-02-22 22:42:37 +0300

    proje yedeğiyle değiştirildi

--- app/src/main/java/com/creative/appcentnasa/modelspirit/NasaResponseSpirit.kt
+++
@@ -1,8 +0,0 @@
-package com.creative.appcentnasa.model
-
-import com.google.gson.annotations.SerializedName
-
-data class NasaResponseSpirit(
-    @SerializedName("photos")
-    val photos: ArrayList<Photo>
-)
